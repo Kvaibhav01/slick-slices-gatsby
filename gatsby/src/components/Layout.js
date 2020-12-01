904490dc5c9c900e922e74db1d1f9574cd22ba13
Vaibhav Khulbe <khulbevaibhavdev@gmail.com>
2020-12-01 20:09:19 +0530

    Added global styles and typography 💅

--- gatsby/src/components/Layout.js
+++ gatsby/src/components/Layout.js
@@ -2,9 +2,15 @@ import React from 'react';
 import Footer from './Footer';
 import Nav from './Nav';
 
+import 'normalize.css';
+import GlobalStyles from '../styles/GlobalStyles';
+import Typography from '../styles/Typography';
+
 export default function Layout({ children }) {
   return (
     <div>
+      <GlobalStyles />
+      <Typography />
       <Nav />
       {children}
       <Footer />
