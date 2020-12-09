e232fc3b833c80515c7ef62c20298c02e150055b
Vaibhav Khulbe <khulbevaibhavdev@gmail.com>
2020-12-09 19:38:01 +0530

    Started Sanity CMS with starting schema ✨

---
+++ sanity/schemas/pizza.js
@@ -0,0 +1,43 @@
+// This file contains all the schema data for our application
+
+import { MdLocalPizza as icon } from 'react-icons/md';
+
+export default {
+  name: 'pizza',
+  title: 'Pizzas',
+  type: 'document',
+  icon,
+  fields: [
+    {
+      name: 'name',
+      title: 'Pizza name',
+      type: 'string',
+      description: 'Name of the pizza',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 100,
+      },
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'price',
+      title: 'Price',
+      type: 'number',
+      description: 'Price of the pizza in cents',
+      validation: (Rule) => Rule.min(1000),
+      // TODO Add custom input component
+    },
+  ],
+};
---
+++ sanity/schemas/topping.js
@@ -0,0 +1,33 @@
+import { FaPepperHot as icon } from 'react-icons/fa';
+
+export default {
+  name: 'topping',
+  title: 'Toppings',
+  type: 'document',
+  icon,
+  fields: [
+    {
+      name: 'name',
+      title: 'Pizza name',
+      type: 'string',
+      description: 'What is the name of the topping?',
+    },
+    {
+      name: 'vegetarian',
+      title: 'Vegetarian',
+      type: 'boolean',
+      options: {
+        layout: 'checkbox',
+      },
+    },
+  ],
+  preview: {
+    select: {
+      name: 'name',
+      vegetarian: 'vegetarian',
+    },
+    prepare: ({ name, vegetarian }) => ({
+      title: `${name} ${vegetarian ? '🌱' : ''}`,
+    }),
+  },
+};
